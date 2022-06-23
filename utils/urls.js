dd8f4dc321846b49450bba3c9310f53b61fef0cc
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2022-06-23 17:01:07 +0300

    revert last push

--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_UPL || 'https://nextjsstrapi.herokuapp.com/'
+export const API_URL = process.env.NEXT_PUBLIC_API_UPL || 'https://smiple-ecommerce-strapi.herokuapp.com'
 //export const API_URL = process.env.NEXT_PUBLIC_API_UPL || 'http://localhost:1337'
 
 export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK || 'pk_test_51IZuTBFpX3iFwiqyV1kXo2dpm19MohvTAPctbwPjEhITSsToCqS9y5oBesWvFx2sFpFojoDz7W7v8Q05MH6wjjkQ00thsYKgTi'
